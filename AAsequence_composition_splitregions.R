01003622d272db8778dfb37570b6dbbad0aa1b01
Andrew Marshall <00101127@uwa.edu.au>
2024-06-04 14:59:47 +0800

    Added feature for automatically including AAs-of-interest ONLY on sliding window plot based on enrichment cutoff value.

--- AAsequence_composition_splitregions.R
+++ AAsequence_composition_splitregions.R
@@ -140,6 +140,15 @@ tibble(
   ) -> box_params
 
 
+#### Normalise composition to aa composition of entire human proteome: ####
+
+enrichment <- cbind(composition$AA,
+                    composition[-1]/proteome_composition$pc)
+
+colnames(enrichment)[1] <- "AA"
+
+
+
 # #### To plot composition for all 20 AAs on different panels: ####
 composition %>%
   # this takes the data from wide format to long format
@@ -161,9 +170,66 @@ composition %>%
 #### To plot composition for all 20 AAs on same graph: ####
 
 # ***OPTION TO ONLY INCLUDE AAs OF INTEREST***:
-aas_to_include <- c(13:14)
 
-composition[aas_to_include,] %>%
+# aas_to_include <- c(1:20)
+
+proportion_cutoff_for_colouring <- 30
+enrichment_cutoff_for_colouring <- 5
+
+aas_to_include <- c()
+for (i in 1:nrow(enrichment)) {
+  if (any(enrichment[i,2:ncol(enrichment)] > enrichment_cutoff_for_colouring)) {
+    aas_to_include <- append(aas_to_include, i)
+  }
+}
+
+# ####To apply colours to significantly enriched AAs only: ####
+# 
+# 
+# if (any(composition[1, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Acolour <- "orange"} else {Acolour <- "grey"}
+# if (any(composition[2, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Ccolour <- "yellow"} else {Ccolour <- "grey"}
+# if (any(composition[3, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Dcolour <- "red"} else {Dcolour <- "grey"}
+# if (any(composition[4, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Ecolour <- "red"} else {Ecolour <- "grey"}
+# if (any(composition[5, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Fcolour <- "green"} else {Fcolour <- "grey"}
+# if (any(composition[6, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Gcolour <- "orange"} else {Gcolour <- "grey"}
+# if (any(composition[7, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Hcolour <- "blue"} else {Hcolour <- "grey"}
+# if (any(composition[8, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Icolour <- "orange"} else {Icolour <- "grey"}
+# if (any(composition[9, 2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Kcolour <- "blue"} else {Kcolour <- "grey"}
+# if (any(composition[10,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Lcolour <- "orange"} else {Lcolour <- "grey"}
+# if (any(composition[11,2:ncol(composition)] > proportion_cutoff_for_colouring)
+#     || any(enrichment[11,2:ncol(enrichment)] > enrichment_cutoff_for_colouring)) {
+#   Mcolour <- "yellow"} else {Mcolour <- "grey"}
+# if (any(composition[12,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Ncolour <- "purple"} else {Ncolour <- "grey"}
+# if (any(composition[13,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Pcolour <- "orange"} else {Pcolour <- "grey"}
+# if (any(composition[14,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Qcolour <- "purple"} else {Qcolour <- "grey"}
+# if (any(composition[15,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Rcolour <- "blue"} else {Rcolour <- "grey"}
+# if (any(composition[16,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Scolour <- "pink"} else {Scolour <- "grey"}
+# if (any(composition[17,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Tcolour <- "pink"} else {Tcolour <- "grey"}
+# if (any(composition[18,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Vcolour <- "orange"} else {Vcolour <- "grey"}
+# if (any(composition[19,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Wcolour <- "green"} else {Wcolour <- "grey"}
+# if (any(composition[20,2:ncol(composition)] > proportion_cutoff_for_colouring)) {
+#   Ycolour <- "green"} else {Ycolour <- "grey"}
+# #######################################
+
+plt2 <- composition[aas_to_include,] %>%
   # this takes the data from wide format to long format
   #   (best to avoid rownames, and instead work explicitly with a column with the 
   #    same ID)
@@ -192,8 +258,46 @@ composition[aas_to_include,] %>%
   
   scale_color_manual(
     values = c(
-      "P" = "darkorange",
-      "Q" = "darkblue"
+      "A" = "orange",
+      "C" = "yellow",
+      "D" = "red",
+      "E" = "red",
+      "F" = "green",
+      "G" = "orange",
+      "H" = "cyan",
+      "I" = "orange",
+      "K" = "blue",
+      "L" = "orange",
+      "M" = "yellow",
+      "N" = "purple",
+      "P" = "hotpink",
+      "Q" = "purple",
+      "R" = "blue",
+      "S" = "pink",
+      "T" = "pink",
+      "V" = "orange",
+      "W" = "green",
+      "Y" = "green"
+      # "A" = Acolour,
+      # "C" = Ccolour,
+      # "D" = Dcolour,
+      # "E" = Ecolour,
+      # "F" = Fcolour,
+      # "G" = Gcolour,
+      # "H" = Hcolour,
+      # "I" = Icolour,
+      # "K" = Kcolour,
+      # "L" = Lcolour,
+      # "M" = Mcolour,
+      # "N" = Ncolour,
+      # "P" = Pcolour,
+      # "Q" = Qcolour,
+      # "R" = Rcolour,
+      # "S" = Scolour,
+      # "T" = Tcolour,
+      # "V" = Vcolour,
+      # "W" = Wcolour,
+      # "Y" = Ycolour
     )
   ) +
   scale_x_continuous(
@@ -205,23 +309,18 @@ composition[aas_to_include,] %>%
   coord_cartesian(
     ylim = c(0, 55)
   ) +
-  # ggplot(aes(as.numeric(position), proportion, color = AA)) + 
-  # # geom_point() +
-  # geom_line(linewidth=2) +
-  # # geom_smooth(linewidth=2, se=FALSE, method="gam", formula = y~poly(x,20)) +
-  # 
   xlab("aa position (centre of window)") +
   ylab("frequency (%)") +
   labs(title = fasta$id, caption = paste("window width = ", window.width)) +
-  theme_classic() -> plt2
+  theme_classic()
 
 ###########################################################################
-#### Normalise composition to aa composition of entire human proteome: ####
-
-enrichment <- cbind(composition$AA,
-                    composition[-1]/proteome_composition$pc)
-
-colnames(enrichment)[1] <- "AA"
+# #### Normalise composition to aa composition of entire human proteome: ####
+# 
+# enrichment <- cbind(composition$AA,
+#                     composition[-1]/proteome_composition$pc)
+# 
+# colnames(enrichment)[1] <- "AA"
 
 
 # #### To plot enrichment for all 20 AAs on different panels: ####
@@ -244,7 +343,7 @@ colnames(enrichment)[1] <- "AA"
 
 #### To plot enrichment for all 20 AAs on same graph: ####
 
-enrichment[aas_to_include,] %>%
+plt3 <- enrichment[aas_to_include,] %>%
   # this takes the data from wide format to long format
   #   (best to avoid rownames, and instead work explicitly with a column with the 
   #    same ID)
@@ -288,12 +387,12 @@ enrichment[aas_to_include,] %>%
   xlab("aa position (centre of window)") +  
   ylab("fold enrichment") +
   labs(title = fasta$id, caption = paste("window width = ", window.width)) + 
-  theme_classic() -> plt3
+  theme_classic()
 
 
 #### patch desired plots together ####
 
 patchwork::wrap_plots(
-  plt2, plt1, ncol = 1
+  plt1, plt2, ncol = 1
   
 )
